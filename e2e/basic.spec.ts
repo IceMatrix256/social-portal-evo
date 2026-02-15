7790bf898bc5e8064f453ca4aebdbeb03b02bc0b
Pandiviri <pandiviri@pop-os.lan>
2026-02-14 20:45:32 -0600

    Fix e2e test title expectation

--- e2e/basic.spec.ts
+++ e2e/basic.spec.ts
@@ -2,5 +2,5 @@ import { test, expect } from '@playwright/test';
 
 test('basic test', async ({ page }) => {
   await page.goto('http://localhost:5173');
-  await expect(page).toHaveTitle(/Social Portal/);
+  await expect(page).toHaveTitle(/social-portal/);
 });

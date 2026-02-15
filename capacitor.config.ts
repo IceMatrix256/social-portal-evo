5137e919124cdd80489044ff018b62ddfeae1411
Pandiviri <pandiviri@pop-os.lan>
2026-02-15 08:22:18 -0600

    Update app name and title to 'Social Portal Evo' for easy distinction

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.icematrix.socialportal.ethereal',
-  appName: 'Social Portal Ethereal',
+  appName: 'Social Portal Evo',
   webDir: 'dist'
 };
 
